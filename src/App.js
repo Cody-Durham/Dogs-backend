8d91989d1a5d185e13800a9c68637b77e46c2755
Cody Durham <codydd@gmail.com>
2021-05-17 14:25:21 -0600

    Frontend Initial commit

--- src/App.js
+++ src/App.js
@@ -5,25 +5,119 @@ import Display from "./Display";
 import Form from "./Form";
 
 function App() {
+
+  //Create a URL in a variable
+  // const url = "http://localhost:4500"
+  const url = "https://dogs-backend-cd.herokuapp.com"
+
+  //Create state to hold list of dogs
+  const [dogs, setDogs] = React.useState([])
+
+  //make new variable for empty dog.. for CREATE FORM 
+  const emptyDog = {
+    name: '', 
+    age: 0, 
+    image: '', 
+  }
+
+  // Setting new state for updating each dog we're going to change/update
+  const [selectedDog, setSelectedDog] = React.useState(emptyDog)
+
+  //Get all dogs function
+  const getDogs = () => {
+    // making a get request from this url
+    fetch(url + "/dog/")
+    // use .then to take action when the response comes in (form line above)
+    .then( (response) => response.json()) // this converts data into an opbjec
+    .then( (data) => { // this will use the data from the response
+      setDogs(data)
+    })
+  }
+  // use effect to use (display) the data right waway
+  React.useEffect( () => {
+    getDogs()
+  }, [])
+
+  // handleCreate funciton =  function for when the coreate form is submitted
+  const handleCreate = (newDog) => {
+    fetch(url + '/dog/', {
+      method: 'POST', 
+      headers: { // this is like the header of a letter
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newDog)// takes the json and makes it a string to send it over
+    })
+    .then( () => getDogs()) // this will get another set of dogs (to render) and update it
+  }
+
+  // hadleUpdate funciton for when the edit form is submitted
+  const handleUpdate = (updatedDog) => {
+    fetch(url + '/dog/' + updatedDog._id, {
+      method: "PUT", 
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(dogs)
+    }) 
+    .then( () => getDogs())
+  }
+ 
+  // function to sepcify which dog were updating (this is setting / updating the STATE)
+  const selectDog = (dog) => {
+    setSelectedDog(dog)
+  }
+
+  // Delete dog funciton to delete
+  const deleteDog = (dog) => {
+    fetch(url + '/dog/' + dog._id, {
+      method: "delete"// this is declaring that its the delete method
+    })
+    .then( () => {
+      getDogs()
+    })
+  }
+
+
   return (
     <div className="App">
       <h1>DOG LISTING SITE</h1>
       <hr />
+      <Link to='/create'>
+        <button>Add a doggie</button>
+      </Link>
       <main>
-        <Switch>
-          <Route exact path="/" render={(rp) => <Display {...rp} />} />
+      <Switch>
+          <Route
+            exact
+            path="/"
+            render={(routerProps) => (<Display {...routerProps} 
+              dogs={dogs} // passing props
+              selectDog={selectDog} // passing props
+              deleteDog={deleteDog} // passing props
+              />
+            )}
+          />
           <Route
             exact
             path="/create"
-            render={(rp) => (
-              <Form {...rp} label="create" dog={{}} handleSubmit={() => {}} />
+            render={(routerProps) => (
+              <Form
+                {...routerProps}
+                label="create"
+                dog={emptyDog}
+                handleSubmit={handleCreate}
+              />
             )}
           />
           <Route
             exact
             path="/edit"
-            render={(rp) => (
-              <Form {...rp} label="update" dog={{}} handleSubmit={() => {}} />
+            render={(routerProps) => (
+              <Form 
+              {...routerProps} 
+              label="update" 
+              dog={selectedDog} 
+              handleSubmit={handleUpdate} />
             )}
           />
         </Switch>
@@ -31,5 +125,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,67 @@
 import React from "react";
 
 const Display = (props) => {
-  return <h1>Display</h1>;
+  //Deconstruct the dogs from props
+  const {dogs} = props
+
+  //Returns the JSX for when you have dogs
+  //mapping over the dogs below
+  const loaded = () => (
+    <div style={{textAlign: "center"}}>
+      {dogs.map((dog) => (
+        <article key={dog._id}>
+          <img src={dog.img}/>
+          <h1>{dog.name}</h1>
+          <h3>{dog.age}</h3>
+          <button onClick={() => {
+            props.selectDog(dog)
+            props.history.push("/edit")
+          }}>
+            edit
+            </button>
+            <button onClick={() => {
+            props.deleteDog(dog)
+          }}>
+            Delete
+          </button>
+        </article>
+      ))}
+    </div>
+  )
+
+  const loading = () => <h1>Loading!..</h1>
+
+  Display ? loaded() : loading()
+  return dogs.length > 0 ? loaded() : loading()
 };
 
+
 export default Display;
+
+
+// const Dog = require("../models/dog");
+// const { Router } = require("express");
+// const router = Router();
+
+// //index route
+// router.get("/", async (req, res) => {
+//   res.json(await Dog.find({}));
+// });
+
+// //create route
+// router.post("/", async (req, res) => {
+//   res.json(await Dog.create(req.body));
+// });
+
+// //update route
+// router.put("/:id", async (req, res) => {
+//   res.json(await Dog.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+// });
+
+// //delete route
+// router.delete("/:id", async (req, res) => {
+//   res.json(await Dog.findByIdAndRemove(req.params.id));
+// });
+
+// // EXPORT ROUTER
+// module.exports = router;
